6e8724cc1f3b543d601754d3bb448db78b1a8cbb
Paweł Prażak <pawel.prazak@gmail.com>
2021-06-07 10:01:55 +0200

    Add package name in node and plugin URL

--- provider/resources.go
+++ provider/resources.go
@@ -125,6 +125,7 @@ func Provider() tfbridge.ProviderInfo {
 		PluginDownloadURL: customPluginDownloadURL(),
 		DataSources:       map[string]*tfbridge.DataSourceInfo{},
 		JavaScript: &tfbridge.JavaScriptInfo{
+			PackageName: "@pawelprazak/pulumi-kind",
 			// List any npm dependencies and their versions
 			Dependencies: map[string]string{
 				"@pulumi/pulumi": "^3.0.0",
