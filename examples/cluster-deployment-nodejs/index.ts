2b2a11bb5f9bbe4171da71f6d19f1bfcf2c1e567
SergiuszRokosz <srokosz@virtuslab.com>
2021-06-16 10:17:51 +0200

    added a js test

---
+++ examples/cluster-deployment-nodejs/index.ts
@@ -0,0 +1,38 @@
+// @ts-ignore because it's provided by yarn link @pawelprazak/pulumi-ind
+import * as kind from "@pawelprazak/pulumi-kind";
+import * as k8s from "@pulumi/kubernetes";
+
+
+
+let cluster = new kind.Cluster("pulumi-cluster", {
+    kindConfig: {
+        kind: "Cluster",
+        apiVersion: "kind.x-k8s.io/v1alpha4",
+        nodes: [
+            {
+                role: "control-plane"
+            },{
+                role: "worker"
+            }
+        ]
+    },
+    waitForReady: true
+});
+
+const appLabels = { app: "nginx" };
+const deployment = new k8s.apps.v1.Deployment("nginx",{
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+            metadata: { labels: appLabels },
+            spec: { containers: [{ name: "nginx", image: "nginx" }] }
+        }
+    }
+},
+{
+    dependsOn: [cluster]
+});
+
+// noinspection JSUnusedGlobalSymbols
+export let clusterName = cluster.name;
